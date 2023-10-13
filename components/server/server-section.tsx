6e70f1269d012826f3481c7f6385b68fe0803d1a
hnsia <44498178+hnsia@users.noreply.github.com>
2023-10-13 23:59:29 +0800

    Setup server channels

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ChannelType, MemberRole } from "@prisma/client";
-import { Plus } from "lucide-react";
+import { Plus, Settings } from "lucide-react";
 
 import { ServerWithMembersWithProfiles } from "@/types";
 import { ActionTooltip } from "@/components/action-tooltip";
@@ -36,6 +36,17 @@ export const ServerSection = ({
                 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
             <Plus className="h-4 w-4" />
+          </button>
+        </ActionTooltip>
+      )}
+      {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionTooltip label="Create Channel" side="top">
+          <button
+            onClick={() => onOpen("members", { server })}
+            className="text-zinc-500 hover:text-zinc-600
+                dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+          >
+            <Settings className="h-4 w-4" />
           </button>
         </ActionTooltip>
       )}
