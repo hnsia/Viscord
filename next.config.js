66515deec0ed4d65bb5651e34cff9df82e4aed04
hnsia <44498178+hnsia@users.noreply.github.com>
2023-11-02 23:24:18 +0800

    Resolve errors from websockets

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["uploadthing.com", "utfs.io"],
   },
+  webpack: (config) => {
+    config.externals.push({
+      bufferutil: "commonjs bufferutil",
+      "utf-8-validate": "commonjs utf-8-validate",
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
